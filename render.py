aa8a88a04e1d3a23d5ce257753502a90eea3a214
Louis Taylor <kragniz@gmail.com>
2013-03-23 22:36:11 +0000

    Removed large comment outlining raytracing

--- render.py
+++ render.py
@@ -1,21 +1,3 @@
-# for each pixel:
-#    * Compute primary ray direction
-#    * Shoot prim ray in the scene
-#    * For each object in the scene:
-#        * Measure the distance to intersection
-#        * Save the object if it's the closest
-#    * If an object was found to intersect:
-#        * Compute illumination
-#        * Find the direction of the shadow rays
-#            * position of light - position of the ray intersection 
-#        * For each object in the scene:
-#            * If the object intersects with the shadow ray:
-#                * Mark the point as in shadow
-#    * If the point is not in shadow:
-#        * Set the pixel value to the colour of the object * brightness of light
-#    * else:
-#        * Set the pixel value to 0
-
 import numpy as np
 from numpy.linalg import norm
 
