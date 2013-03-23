30769f302b5fa19ce88686b5ce464e9f96b126f8
Louis Taylor <kragniz@gmail.com>
2013-03-23 20:15:54 +0000

    Conform to PPM file format

--- pixmap.py
+++ pixmap.py
@@ -3,17 +3,17 @@ def save(data, filename):
     hight = len(data[0])
 
     outFile = open(filename, 'w')
-    outFile.write('P3\n{width} {hight}\n'.format(width=width, hight=hight))
+    outFile.write('P3\n{hight} {width}\n255\n'.format(width=width, hight=hight))
 
     for i in data:
         for j in i:
-            outFile.write('{} {} {}  '.format(*j))
-        outFile.write('\n')
+            outFile.write('{}\n{}\n{}\n'.format(*j))
+        #outFile.write('\n')
 
 if __name__ == '__main__':
     import numpy as np
     image = np.array([
-        np.array([np.array([255,255,255]),np.array([255,255,255]),np.array([255,255,255])]),
+        np.array([np.array([255,0,0]),np.array([0,255,0]),np.array([0,0,255])]),
         np.array([np.array([255,255,255]),np.array([255,255,255]),np.array([255,255,255])]),
         np.array([np.array([255,255,255]),np.array([255,0,0]),np.array([255,255,255])]),
         np.array([np.array([255,255,255]),np.array([255,255,255]),np.array([255,255,255])]),
