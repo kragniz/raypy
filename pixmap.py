85bf47c076e58f66bab08eec2bc041c299071d8b
Louis Taylor <kragniz@gmail.com>
2013-03-27 10:06:34 +0000

    Added configurable maxvalue

--- pixmap.py
+++ pixmap.py
@@ -1,14 +1,17 @@
-def save(data, filename):
+def save(data, filename, maxValue=2**16-1):
     width = len(data)
     hight = len(data[0])
 
     outFile = open(filename, 'w')
-    outFile.write('P3\n{hight} {width}\n4096\n'.format(width=width, hight=hight))
+    outFile.write('P3\n{hight} {width}\n{max}\n'.format(
+                      width=width,
+                      hight=hight,
+                      max=maxValue)
+                 )
 
     for i in data:
         for j in i:
             outFile.write('{}\n{}\n{}\n'.format(*j))
-        #outFile.write('\n')
 
 if __name__ == '__main__':
     import numpy as np
